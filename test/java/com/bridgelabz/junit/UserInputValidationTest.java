b31236269e85f5cef200743175ccb8e6c25659fb
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-22 15:51:17 +0530

    Added:  Test case to check whether user has entered valid email

--- test/java/com/bridgelabz/junit/UserInputValidationTest.java
+++ test/java/com/bridgelabz/junit/UserInputValidationTest.java
@@ -17,17 +17,30 @@ public class UserInputValidationTest {
         boolean result = userInputValidation.isValidFirstName("Pr");
         Assert.assertEquals(false, result);
     }
-    @org.junit.jupiter.api.Test
+    @Test
     public void givenLastName_WhenProper_ShouldReturnTrue() {
         UserInputValidation userInputValidation = new UserInputValidation();
         boolean result = userInputValidation.isValidLastName("Hawaldar");
         Assert.assertEquals(true, result);
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     public void givenLastName_WhenShort_ShouldReturnFalse() {
         UserInputValidation userInputValidation = new UserInputValidation();
         boolean result = userInputValidation.isValidLastName("So");
+        Assert.assertEquals(false, result);
+    }
+    @Test
+    public void givenEmail_WhenValid_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("hawaldarravindra@gmail.com");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenEmail_WhenNotValid_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidEmail("Ravindra..675.gmail.com");
         Assert.assertEquals(false, result);
     }
 }
