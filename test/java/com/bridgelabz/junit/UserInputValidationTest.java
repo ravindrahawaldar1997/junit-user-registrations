44093c7d77b24c8db48b2daa6bc21974edc1c1f4
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-22 17:02:49 +0530

    Added:  Test case to check whether user has entered valid password

--- test/java/com/bridgelabz/junit/UserInputValidationTest.java
+++ test/java/com/bridgelabz/junit/UserInputValidationTest.java
@@ -56,4 +56,17 @@ public class UserInputValidationTest {
         boolean result = userInputValidation.isValidMobileNumber("908765-4321908");
         Assert.assertEquals(false, result);
     }
+    @Test
+    public void givenPassword_WhenProper_ShouldReturnTrue() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidMobileNumber("91 9087654321");
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenPassword_WhenNotProper_ShouldReturnFalse() {
+        UserInputValidation userInputValidation = new UserInputValidation();
+        boolean result = userInputValidation.isValidMobileNumber("908765-4321908");
+        Assert.assertEquals(false, result);
+    }
 }
