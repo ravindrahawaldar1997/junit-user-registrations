cbeb0295ffc907e4dbea54f9bbd5f4f57340d324
ravindrahawaldar1997 <hawaldarravindra@gmail.com>
2022-12-23 10:18:34 +0530

    Added: program to check whether the given mail is valid

---
+++ main/java/com/bridgelabz/junit/EmailPatternValidation.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.junit;
+import java.util.regex.Pattern;
+public class EmailPatternValidation {
+    public boolean isValidEmail(String email) {
+        //Regex to check first mandatory part i.e; abc, @, bridgelabz, ".", co and xyz and .in
+        String emailRegex = "^[a-zA-Z0-9_+-]+(?:\\.[a-zA-Z0-9_+-]+)*@" +
+                "([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}" +
+                "(?:(\\.([A-Za-z]{2})))?$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pattern.matcher(email).matches();
+    }
+}
